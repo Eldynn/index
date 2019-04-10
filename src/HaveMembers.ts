746adc7630fb85632eba6888c0d7488ed3207cf7
Robin BAILLARGEAUX <baillargeaux.robin@gmail.com>
2019-04-10 18:41:45 +0200

    Fix accessibility for tests

--- src/HaveMembers.ts
+++ src/HaveMembers.ts
@@ -2,7 +2,7 @@ import { Identity } from './Identity';
 import { Member } from './Member';
 
 export abstract class HaveMembers extends Identity {
-  private members: Set<Member> = new Set<Member>();
+  public members: Set<Member> = new Set<Member>();
 
   public add(member: Member): void {
     this.members.add(member);
