bd642e42f5a5512543cc86128ae90afdf5a0d62c
Robin BAILLARGEAUX <baillargeaux.robin@gmail.com>
2019-04-20 06:45:03 +0200

    Add script docs to overload tplant

---
+++ scripts/docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "${0}")
+
+cat ${BASEDIR}/links.puml >> ${BASEDIR}/../docs/index.puml
+
+perl -0777 -i~ -pe "s/\@enduml\r?\n\@startuml//" ${BASEDIR}/../docs/index.puml
+
+rm ${BASEDIR}/../docs/index.puml~
