d127175a66075253abe74764bba9959de9d6d9d6
Robin BAILLARGEAUX <baillargeaux.robin@gmail.com>
2019-04-22 18:31:29 +0200

    build(rollup): Transform rollup config file to typescript

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import typescript from 'rollup-plugin-typescript2';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
